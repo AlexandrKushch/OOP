eb15699fad5d7dde0be85508e9e2fe5a04580a88
28b-kushch <a.kushch0@gmail.com>
2020-06-08 19:30:07 +0300

    Fix load dates

--- src/ua/khpi/oop/kushch09/Main.java
+++ src/ua/khpi/oop/kushch09/Main.java
@@ -2,18 +2,21 @@ package ua.khpi.oop.kushch09;
 
 import java.awt.image.AreaAveragingScaleFilter;
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         ListLink<Human> humans = new ListLink<>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
-        humans.add(new Human("Danya", 21, new Date("1999-06-06"), true));
-        humans.add(new Human("Dasha", 20, new Date("2000-10-26"), false));
-        humans.add(new Human("Misha", 39, new Date("1981-03-13"), true));
-        humans.add(new Human("Sasha", 19, new Date("2001-06-15"), true));
+        humans.add(new Human("Danya", 21, simpleDateFormat.parse("06-06-1999"), true));
+        humans.add(new Human("Dasha", 20, simpleDateFormat.parse("26-10-2000"), false));
+        humans.add(new Human("Misha", 39, simpleDateFormat.parse("13-03-1981"), true));
+        humans.add(new Human("Sasha", 19, simpleDateFormat.parse("15-06-2001"), true));
 
         for (Human h : humans) {
             System.out.println(h.getName());
